5065e48474dbb6edf4e5de2bb3a919b562de382a
sairampandiri <41752834+sairampandiri@users.noreply.github.com>
2018-07-29 16:14:35 +0530

    Update twoodd.py

--- twoodd.py
+++ twoodd.py
@@ -1,5 +1,5 @@
 # Given two numbers display odd numbers between two intervals
 n,s=map(int,input().split())
-for i in range(n,s):
+for i in range(n+1,s):
 	if((i%2==1)and(i<=100000)):
 		print(i,end=" ")
