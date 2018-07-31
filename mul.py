2d9e1c37b9a19619538f4032382f393e391bd214
sairampandiri <41752834+sairampandiri@users.noreply.github.com>
2018-07-31 10:43:25 +0530

    Update mul.py

--- mul.py
+++ mul.py
@@ -1,6 +1,9 @@
 # given a number and multiply 5 times
 n=int(input())
 i=1
-while(i<=5):
-	print(n*i,end=" ")
+for i in range(1,6):
+	if(i<6):
+		print(n*i,end=" ")
+	else:
+		print(n*i,end="")
 	i+=1
