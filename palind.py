e3de203ec2cac83a7839ca484ecc5dcee6155749
sairampandiri <41752834+sairampandiri@users.noreply.github.com>
2018-07-31 13:55:22 +0530

    Update palind.py

--- palind.py
+++ palind.py
@@ -2,13 +2,11 @@
 n=int(input())
 temp=n
 b=0
-if(n<=1000):
-	#for i in range(0,n):
-	while(n>0):
-		a=n%10
-		b=b*10+a
-		n=n//10
-		
+#for i in range(0,n):
+while(n>0):
+	a=n%10
+	b=b*10+a
+	n=n//10
 if(temp==b):
 	print("yes")
 else:
