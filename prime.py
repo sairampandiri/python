a526566e8a96c208203da79aaabe1da693a901fd
sairampandiri <41752834+sairampandiri@users.noreply.github.com>
2018-08-06 15:30:40 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -4,11 +4,11 @@ if a<=1000:
 	if a>1:
 		for i in range(2,a):
 			if (a%i)==0:
-				print("no")
+				print("no",end="")
 				break
 		else:
-			print("yes")
+			print("yes",end="")
 	else:
-		print("yes")
+		print("yes",end="")
 else:
-	print("no")
+	print("no",end="")
