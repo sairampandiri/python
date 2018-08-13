6134cefa501bbec879b28a085fc780563dc3eed2
sairampandiri <41752834+sairampandiri@users.noreply.github.com>
2018-08-13 16:52:11 +0530

    Create abc.py

---
+++ abc.py
@@ -0,0 +1,10 @@
+a=[]
+n=int(input("Enter number of elements:"))
+if 1<=n<=1000:
+	for i in range(1,n+1):
+		b=int(input("Enter element:"))
+		a.append(b)
+		a.sort()
+		a(int(a))
+print(a)
+#print("Largest element is:",a[n-1])
