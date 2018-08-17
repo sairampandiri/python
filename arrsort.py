d656ac89cc0ddc1b5f2b9e7237c9291094ad05d8
sairampandiri <41752834+sairampandiri@users.noreply.github.com>
2018-08-17 20:57:50 +0530

    Update arrsort.py

--- arrsort.py
+++ arrsort.py
@@ -1,6 +1,17 @@
 # sort array of n element
 n=int(input())
+c=0
 a = [int(x) for x in input().split()]
 if 1<=n<=1000:
 	a.sort()
-	print(a,end="")
+for i in range(0,len(a)):
+		if(i==c):
+			c+=1
+			if(c==1):
+				k=""
+			else:
+				k=" "
+		else:
+			k=""
+		print(end=k)
+		print(a[i],end="")
