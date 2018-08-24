c44f2b09aa1f2b24c0a12803a8a4a7988f70c056
sairampandiri <41752834+sairampandiri@users.noreply.github.com>
2018-08-24 09:34:37 +0530

    Create median.py

---
+++ median.py
@@ -0,0 +1,11 @@
+# median element
+n=int(input())
+b=[int(x) for x in input().split()]
+c=b.sort()
+print(b)
+if 1<=n<=1000:
+  mn=int(n/2)
+  if(n%2==0):
+    print(b[mn-1],b[mn])
+  else:
+    print(b[mn])
