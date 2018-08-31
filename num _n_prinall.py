3561463f42fe1da2497f8c0a01eaf9b0acf502ce
sairampandiri <41752834+sairampandiri@users.noreply.github.com>
2018-08-31 16:04:07 +0530

    Update num _n_prinall.py

--- num _n_prinall.py
+++ num _n_prinall.py
@@ -5,11 +5,5 @@ for a in range(0,len(y)):
   if(a<len(y)-1):
     print(y[a],end=" ")
   else:print(y[a],end="")'''
-'''l=input("Enter the value:")
-print(" j".join(str(x) for x in l))'''
-N=int(input())
-n=N%10
-m=N//10
-g=m%10
-h=m//10
-print(h,g,n)
+l=input()
+print(" ".join(str(x) for x in l))
