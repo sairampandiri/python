8c429f130732a9ec74b684902081bdfe65c2e479
sairampandiri <41752834+sairampandiri@users.noreply.github.com>
2018-09-01 15:52:01 +0530

    Create check_numandalpha.py

---
+++ check_numandalpha.py
@@ -0,0 +1,14 @@
+#given a string s, check if contains numeric and alphabets
+m=input("Enter Alphanumeric")
+c=0
+d=0
+for i in m:
+    if(i.isdigit()):
+      c=c+1
+    if(i.isalpha()):
+      d=d+1
+    if(c>0 and d>0):
+        print('yes')
+        break
+if(c==0 or d==0):
+  print('No')
