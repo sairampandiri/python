cdc486f293ed1a3e00ff89a4a72bcb329febb2fa
sairampandiri <41752834+sairampandiri@users.noreply.github.com>
2018-09-06 12:40:17 +0530

    Update num_to_wor.py

--- num_to_wor.py
+++ num_to_wor.py
@@ -1,7 +1,11 @@
 #given a number n,print it in words
-import inflect
+'''import inflect
 import inflection
 n=int(input())
 p = inflect.engine()
 c=p.number_to_words(n)
-print(inflection.camelize(c))
+print(inflection.camelize(c))'''
+a=int(input())
+lst=['One','Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten']
+if(1<=a<=10):
+  print(lst[a-1])
