eb12cfef78cb628da9280df1be5095a9951a844d
sairampandiri <41752834+sairampandiri@users.noreply.github.com>
2018-09-07 19:45:11 +0530

    Update check_a_in 2numb.py

--- check_a_in 2numb.py
+++ check_a_in 2numb.py
@@ -1,6 +1,6 @@
 #Given a number N and 2 numbers .Check whether N is between L and R.print yes/no
-c=int(input())
-a,b=map(int,input().split())
+b=int(input())
+a,c=map(int,input().split())
 if(a<b<c):
   print("yes")
 else:
