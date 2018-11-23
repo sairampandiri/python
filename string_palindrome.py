92094e7a0c9a0684bf36715321ad2fb2f6385913
sairampandiri <41752834+sairampandiri@users.noreply.github.com>
2018-11-23 16:07:14 +0530

    Create string_palindrome.py

---
+++ string_palindrome.py
@@ -0,0 +1,11 @@
+#Given a string s ,check the palindrome.print yes/no
+a="amma"
+b=a[::-1]
+if(a==b):
+  print("yes")
+else:
+  print("no")
+'''b=""
+for i in range(len(a)-1,-1,-1):
+  b=b+a[i]
+print(a.reverse())'''
