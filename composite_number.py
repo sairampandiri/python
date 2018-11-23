937b8ee9ab7c911b5e2310c5093aff846534d563
sairampandiri <41752834+sairampandiri@users.noreply.github.com>
2018-11-23 19:32:04 +0530

    Update composite_number.py

--- composite_number.py
+++ composite_number.py
@@ -1,7 +1,7 @@
 #Given anuber a N,check wheather it is a composite or not
-n=int(input())
-for i in range(2,n):
-  if(n%i)==0:
+num=int(input())
+for k in range(2,num):
+  if (num%k)==0:
     print("yes")
     break
 else:
