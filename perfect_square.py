861fdc58127d39e7e6343d9584cfc4932d522d58
sairampandiri <41752834+sairampandiri@users.noreply.github.com>
2018-11-24 07:16:42 +0530

    Update perfect_square.py

--- perfect_square.py
+++ perfect_square.py
@@ -1,10 +1,13 @@
 #Given two numbers N,M.Find their product and check whether it is a perfect square
 a,b=map(int,input().split())
 n=a*b
-for i in range(1,n):
-    if i*i==n:
-      print("yes")
-      break
+if(n==0):
+  print("yes")
 else:
-  print("no")
+  for i in range(0,n):
+      if i*i==n:
+        print("yes")
+        break
+  else:
+    print("no")
     
