b465a0a54618215b73579c8020e03eb25684edf8
sairampandiri <41752834+sairampandiri@users.noreply.github.com>
2018-11-24 07:24:04 +0530

    Update chan_midd_ele_*.py

--- chan_midd_ele_*.py
+++ chan_midd_ele_*.py
@@ -1,11 +1,11 @@
-#given a string s,print it after changing middle element to * (if the length of the string is even ,change the middle elements to *)
+#given a string s,print it after changingmiddle element to * (if the length of the string is even ,change the middle elements to *)
 a=input()
-if(len(a)%2)==1:
+if len(a)%2==1:
   b=len(a)//2
-  a=a.replace(a[b],'*')
+  a=a.replace(a[b],'*',1)
   print(a)
 else:
   b=len(a)//2
-  a=a.replace(a[b],'*')
-  a=a.replace(a[b-1],'*')
+  a=a.replace(a[b],'*',1)
+  a=a.replace(a[b-1],'*',1)
   print(a)
